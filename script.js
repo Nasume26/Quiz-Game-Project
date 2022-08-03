20a88782d3b9bea5022e10fe27737c1f15d05817
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-03 15:56:06 -0500

    Removed template and commented out in JS file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //add query selector to button -> add event listener to button.
+const start = document.querySelector(".start")
+
+
+
+
 //when start button is pressed, create question 1 (deleting the start button in the process.) ->
 //there should be a bottom line with a p tag, centered in the p tag (or right aligned) will be three O's
 //whenever an incorrect answer is recieved a span loses an O (use if statements to determine if one has been
@@ -9,3 +14,27 @@
 
 
 //create an array called Questions -> populate the array with your question information.
+
+
+
+// <div class="quizContainer">
+//             <div class="imageContainer">
+//                 <img src="" alt="IMAGE TO DISPLAY HERE" class="displayImage">
+//             </div>
+//             <div class="buttonContainer">
+//                 <div class="buttonRowOne">
+//                     <button class="button buttonOne correctAnswer">B1C</button> <button class="button buttonTwo incorrectAnswer">B2</button>
+//                 </div>
+//                 <div class = "buttonRowTwo">
+//                     <button class="button buttonThree incorrectAnswer">B3</button> <button class="button buttonFour incorrectAnswer">B4</button> 
+//                 </div>
+//             </div>
+//             <div class="scoreContainer">
+//                 <div class="livesRemaining">
+//                     <p>OOO</p>
+//                 </div>
+//                 <div class="scoreTally">    
+//                     <p>Score:</p>
+//                 </div>
+//             </div>
+//         </div>
