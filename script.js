5ae1609618db59b637820c62ccfd48ebea466c58
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-03 15:59:24 -0500

    The start button now loads the quiz card template

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //add query selector to button -> add event listener to button.
 const start = document.querySelector(".start")
+const quizCard = document.querySelector(".quizCard")
 
-
+start.addEventListener("click", () => {
+    quizCard.innerHTML = `
+    <div class="quizContainer">
+            <div class="imageContainer">
+                <img src="" alt="IMAGE TO DISPLAY HERE" class="displayImage">
+            </div>
+            <div class="buttonContainer">
+                <div class="buttonRowOne">
+                    <button class="button buttonOne correctAnswer">B1C</button> <button class="button buttonTwo incorrectAnswer">B2</button>
+                </div>
+                <div class = "buttonRowTwo">
+                    <button class="button buttonThree incorrectAnswer">B3</button> <button class="button buttonFour incorrectAnswer">B4</button> 
+                </div>
+            </div>
+            <div class="scoreContainer">
+                <div class="livesRemaining">
+                    <p>OOO</p>
+                </div>
+                <div class="scoreTally">    
+                    <p>Score:</p>
+                </div>
+            </div>
+        </div>`
+})
 
 
 //when start button is pressed, create question 1 (deleting the start button in the process.) ->
@@ -17,24 +41,3 @@ const start = document.querySelector(".start")
 
 
 
-// <div class="quizContainer">
-//             <div class="imageContainer">
-//                 <img src="" alt="IMAGE TO DISPLAY HERE" class="displayImage">
-//             </div>
-//             <div class="buttonContainer">
-//                 <div class="buttonRowOne">
-//                     <button class="button buttonOne correctAnswer">B1C</button> <button class="button buttonTwo incorrectAnswer">B2</button>
-//                 </div>
-//                 <div class = "buttonRowTwo">
-//                     <button class="button buttonThree incorrectAnswer">B3</button> <button class="button buttonFour incorrectAnswer">B4</button> 
-//                 </div>
-//             </div>
-//             <div class="scoreContainer">
-//                 <div class="livesRemaining">
-//                     <p>OOO</p>
-//                 </div>
-//                 <div class="scoreTally">    
-//                     <p>Score:</p>
-//                 </div>
-//             </div>
-//         </div>
