1f542fb7fde86ef97b9957262f358d0e5ced7ec0
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-05 10:55:05 -0500

    Began reformatting Code for readability

--- script.js
+++ script.js
@@ -226,22 +226,3 @@ const startButtonOnReset = () => {
             })
         
 }
-
-
-
-
-
-
-//when start button is pressed, create question 1 (deleting the start button in the process.) ->
-//there should be a bottom line with a p tag, centered in the p tag (or right aligned) will be three O's
-//whenever an incorrect answer is recieved a span loses an O (use if statements to determine if one has been
-//already lost. When all three lives are lost reset the entire Div to a HUGE game over screen.)
-//define all necessary classes.
-//Define a variable called Question NUmber. This will control the arrays information positioning on succesful 
-//answer clicks.
-
-
-//create an array called Questions -> populate the array with your question information.
-
-
-
